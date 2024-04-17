ff61e5ea2deb5cbfa8330deff9e68a5b376639cd
Treehugger255 <ethan.eschrich@gmail.com>
2024-04-17 03:43:12 -0400

    Correct reduce_dictionary.py to correctly reduce a given dictionary using DFS and a max reduced size

--- reduce_dictionary.py
+++ reduce_dictionary.py
@@ -2,14 +2,9 @@ import os
 import json
 import random
 from graph_utils import load_graph_dict, get_num_vertices
-from typing import Dict, List, Union, Tuple
 
+import collections
 
-def add_recursive(full_graph: Dict[int, List], reduced_graph: Dict[int, List], index : int) -> None:
-    for dest in full_graph[index]:
-        if dest not in reduced_graph.keys():
-            reduced_graph[dest] = full_graph[dest]
-            add_recursive(full_graph, reduced_graph, dest)
 
 
 if __name__ == '__main__':
@@ -21,11 +16,7 @@ if __name__ == '__main__':
                         help='path to dir where the results and graph are stored')
     parser.add_argument('--N', type=int,
                         default=1000,
-                        help='Number of experiments to run')
-    parser.add_argument('--n_cores', type=int,
-                        default=12,
-                        help='Num cores to use'
-                        )
+                        help='Max number of words in reduced dictionary')
     parser.add_argument('--seed', type=int,
                         default=2,
                         help='random seed to use'
@@ -33,15 +24,48 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    # Set seed
+    random.seed(args.seed)
+
+    # Load graph from given directory
     graph = load_graph_dict(os.path.join(args.load_dir, "graph.json"))
     num_vertices = get_num_vertices(os.path.join(args.load_dir, "encoding_dict.json"))
-    reduced_keys = random.sample(graph.keys(), 1000)
-    reduced_graph = {key:graph[key] for key in reduced_keys}
 
-    for v in reduced_keys:
-        add_recursive(graph, reduced_graph, v)
+    # Initialize sets
 
-    with open("russian_reduced.json", "w", encoding="utf-8") as f:
+    graph_vertices = set(graph.keys())
+    visited = set()
+
+    # Depth-first search.  Done because we want to prioritize deeper words, i.e. closer to primes, rather than
+    # More "higher" level words
+    # Adapted from https://www.geeksforgeeks.org/iterative-depth-first-traversal/
+
+    # While we haven't reached the total yet, perform a new depth-first search
+    while len(visited) < args.N:
+        # Choose a new random vertex to start depth-searching from
+        root = random.choice(list(graph_vertices - visited))
+
+        # Create a stack for DFS
+        stack = collections.deque(root)
+
+        while (len(stack)) and len(visited) < args.N:
+            # Pop a vertex from stack
+            v = stack[-1]
+            stack.pop()
+
+            # Stack may contain same vertex twice. So
+            # we need to print the popped item only
+            # if it is not visited.
+            if v not in visited:
+                visited.add(v)
+
+            # Get all adjacent vertices of the popped vertex s
+            # If a adjacent has not been visited, then push it
+            # to the stack.
+            for neighbor in graph[v]:
+                if neighbor not in visited:
+                    stack.append(neighbor)
+
+    # Save resulting dictionary
+    with open("graph_reduced.json", "w", encoding="utf-8") as f:
         json.dump(graph, f, ensure_ascii=False)
-
-    num_vertices = get_num_vertices(os.path.join(args.load_dir, "encoding_dict.json"))
